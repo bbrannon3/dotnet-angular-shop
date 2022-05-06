58985d49a76bf021c70c5506e75995ea8134a7b9
Brody Brannon <brody@redpointegroup.com>
2022-05-06 08:40:14 -0500

    End of Section 12

---
+++ client/src/app/core/section-header/section-header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BreadcrumbService } from 'xng-breadcrumb';
+
+@Component({
+  selector: 'app-section-header',
+  templateUrl: './section-header.component.html',
+  styleUrls: ['./section-header.component.scss']
+})
+export class SectionHeaderComponent implements OnInit {
+
+  constructor(private breadService: BreadcrumbService) { }
+  breadcrumb$: Observable<any[]>;
+  ngOnInit(): void {
+    this.breadcrumb$ = this.breadService.breadcrumbs$;
+  }
+
+}
